8f0b638fec642f923c84656d59845c6bcc6244d8
Jan Lubeck <jan@fyrastudio.com>
2016-09-08 15:22:18 -0400

    Initial Commit

---
+++ FSUtils/FSUtils.h
@@ -0,0 +1,19 @@
+//
+//  FSUtils.h
+//  FSUtils
+//
+//  Created by Jan on 9/8/16.
+//  Copyright © 2016 Fyrastudio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FSUtils.
+FOUNDATION_EXPORT double FSUtilsVersionNumber;
+
+//! Project version string for FSUtils.
+FOUNDATION_EXPORT const unsigned char FSUtilsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FSUtils/PublicHeader.h>
+
+
